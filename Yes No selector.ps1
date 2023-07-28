f6f16badf7d7fa648544ed715e0393984e8e23a7
Chris Saladin <80872220+ChrisSaladin@users.noreply.github.com>
2023-07-27 22:20:13 -0500

    Update Yes No selector.ps1

--- Yes No selector.ps1
+++ Yes No selector.ps1
@@ -1,5 +1,8 @@
 # simple yes / no selector to add to scripts. 
 # OP of this was found here: https://stackoverflow.com/questions/65454348/how-can-i-allow-only-y-n-to-be-input-in-read-host-without-having-to-press-enter
+# works in pwsh and powershell5
+# powershell.exe -command "Invoke-expression (invoke-webrequest -usebasicparsing).content"
+# pwsh.exe -command "Invoke-expression (invoke-webrequest -usebasicparsing).content"
 
 # Yes or No checker
 
